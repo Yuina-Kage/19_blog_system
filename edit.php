3839f8aa3da5cf1e10f7fcb23d0d271fe9b3eabe
Yuina-Kage <yuina.kg@gmail.com>
2020-03-23 11:37:27 +0900

    ブログ更新機能作成

--- edit.php
+++ edit.php
@@ -4,19 +4,27 @@ require_once('config.php');
 require_once('functions.php');
 
 session_start();
-$dbh = connectDb();
 
-$sql = "select * from categories";
+$id = $_GET['id'];
+if(!is_numeric($id)) {
+  header('Location: index.php');
+  exit;
+}
+
+// わざとこの位置
+$dbh = connectDb(); 
+
+$sql = "select * from posts where id = :id";
 $stmt = $dbh->prepare($sql);
+$stmt->bindParam(":id", $id, PDO::PARAM_INT);
 $stmt->execute();
+$post = $stmt->fetch(PDO::FETCH_ASSOC);
 
-$categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $title = $_POST['title'];
   $body = $_POST['body'];
   $category_id = $_POST['category_id'];
-  $user_id = $_SESSION['id'];
 
   $errors = [];
 
@@ -33,24 +41,41 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   }
 
   if(empty($errors)) {
-    $sql = "insert into posts " . 
-      "(title, body, category_id, user_id, created_at, updated_at) values " . 
-      "(:title, :body, :category_id, :user_id, now(), now())";
+    $sql = <<<SQL
+    
+    update
+      posts
+    set
+      title = :title,
+      body = :body,
+      category_id = category_id
+    where
+      id = :id  
+    SQL;
+
     $stmt = $dbh->prepare($sql);
     
     $stmt->bindParam(':title', $title, PDO::PARAM_STR);
     $stmt->bindParam(':body', $body, PDO::PARAM_STR);
     $stmt->bindParam(':category_id', $category_id, PDO::PARAM_INT);
-    $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
     
     $stmt->execute();
-    
-    // // 登録直後のID取得
-    $id = $dbh->lastInsertId();
     header("Location: show.php?id={$id}");
     exit;
   }
 }
+
+if(empty($post)) {
+  header('Location: index.php');
+  exit;
+}
+
+$sql = "select * from categories";
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+
+$categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
@@ -95,7 +120,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <div class="col-sm-11 col-md-9 col-lg-7 mx-auto">
           <div class="card card-signin my-5 bg-light">
             <div class="card-body">
-              <h5 class="card-title text-center">新規記事</h5>
+              <h5 class="card-title text-center">記事編集</h5>
               <?php if ($errors) : ?>
                 <ul class="alert alert-danger">
                   <?php foreach ($errors as $error) : ?> 
@@ -106,43 +131,40 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
               <form action="new.php" method="post">
                 <div class="form-group">
                   <label for="title">タイトル</label>
-                  <input type="text" name="title" id="" class="form-control" autofocus required>
+                  <input type="text" name="title" id="" class="form-control" autofocus required value="<?php echo h($post['title']); ?>">
                 </div>
                 <div class="form-group">
                   <label for="category_id">カテゴリー</label>
                   <select name="category_id" class="form-control" required>
-                    <option value="" disabled selected>選択して下さい</option>
+                    <option value="" disabled>選択して下さい</option>
                     <?php foreach ($categories as $c)  :?>
-                      <option value="<?php echo h($c['id']); ?>"><?php echo h($c['name']); ?>
-                    </option>
-                    <?php endforeach; ?>
-                  </select>
-                </div>
-                <div class="form-group">
-                  <label for="body">本文</label>
-                  <textarea name="body" id="" cols="30" rows="10" class="form-control required"></textarea>
-                </div>
-                <div class="form-group">
-                  <input type="submit" value="登録" class="btn btn-lg btn-primary btn-block">
-                </div>
-              </form>
+                    <!-- 三項演算子↓(シンプルな時に使う) -->
+                      <option value="<?php echo h($c['id']); ?>"
+                        <?php echo $post['category_id'] == $c['id'] ? "selected" : "" ?>><?php echo h($c['name']); ?>
+                      </option>
+                      <?php endforeach; ?>
+                    </select>
+                  </div>
+                  <div class="form-group">
+                    <label for="body">本文</label>
+                    <textarea name="body" id="" cols="30" rows="10" class="form-control required"><?php echo h($post['body']); ?></textarea>
+                  </div>
+                  <div class="form-group">
+                    <input type="submit" value="更新" class="btn btn-lg btn-primary btn-block">
+                  </div>
+                </form>
+                <a href="show.php?id=<?php echo h($post['id']);?>" class="btn btn-lg btn-info btn-block">戻る</a>
+              </div>
             </div>
           </div>
         </div>
       </div>
+      <footer class="footer font-small bg-dark">
+        <div class="footer-copyright text-center py-3 text-light">&copy; 2020 Camp Blog</div>
+      </footer>
     </div>
-    <footer class="footer font-small bg-dark">
-      <div class="footer-copyright text-center py-3 text-light">&copy; 2020 Camp Blog</div>
-
-    </footer>
-  </div>
-</body>
-</html>
-
-
-
-
-
-
-
-
+  </body>
+  </html>
+                    
+  
+                      
